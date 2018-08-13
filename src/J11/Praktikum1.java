6c6fe4667406ced9164ff5b41a47234a4e029506
susan <susan@asus>
2018-08-13 20:06:08 +0700

    Coba Method contains,toUpperCase, toLowerCase, trim & concat

--- src/J11/Praktikum1.java
+++ src/J11/Praktikum1.java
@@ -46,6 +46,16 @@ public static void main(String[] args){
     System.out.println("Isi variabel s : " + s);
     System.out.println("Perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
     
+    System.out.println("Pada x terdapat huruf 'a' : " + x.contains("a"));
+    
+    System.out.println("Isi variabel x besar semua : " + x.toUpperCase());
+    System.out.println("Isi variabel x kecil semua : " + x.toLowerCase());
+    
+    String t = " Operasi String ";
+    System.out.println("Isi variabel t : \""+ t + "\"");
+    System.out.println("Isi variabel t tanpa spasi : \""+ t.trim() + "\"");
+    
+    System.out.println("Gabungkan isi variabel x dan r : "+ x.concat(r));
    
 }
 }    
