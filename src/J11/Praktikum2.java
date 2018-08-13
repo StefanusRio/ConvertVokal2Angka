8ce787f0e232671522e869214712167a40c3eebf
susan <susan@asus>
2018-08-13 20:21:45 +0700

    Coba Method Initial commit

---
+++ src/J11/Praktikum2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author susan
+ */
+import java.util.Scanner;
+public class Praktikum2 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas: " + identitas);
+        System.out.println("\nCovert Kalimat Alay Angka (Vokal ke Angka)\n");
+    }
+    }
+    
+}
