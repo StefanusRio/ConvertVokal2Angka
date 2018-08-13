8087c6cc35c13edd1587558953bf8df69519cefd
susan <susan@asus>
2018-08-13 20:00:12 +0700

    Coba Method length & isEmpty

--- src/J11/Praktikum1.java
+++ src/J11/Praktikum1.java
@@ -14,5 +14,17 @@ public class Praktikum1 {
 public static void main(String[] args){
     String identitas = "Stefanus Ario Prabowo / X RPL 3 / 37";
     System.out.println("Identitas: " + identitas);
-}
+    
+    String x = "Operasi";
+    System.out.println("Isi variabel x : " + x);
+    System.out.println("\"" + x + "\" panjang karakter = "+ x.length());
+    
+    System.out.println("x adalah kosong : " + x.isEmpty());
+    
+    String y ="";
+    System.out.println("Isi variabel y : " + y);
+    System.out.println("y adalah kosong : " + y.isEmpty());
+    
+    
+}    
 }
